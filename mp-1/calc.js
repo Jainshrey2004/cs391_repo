188280e5460bd959a72796fde092cbfeeb15e251
Jainshrey2004 <144143077+Jainshrey2004@users.noreply.github.com>
2025-09-16 13:25:10 -0400

    Update calc.js

--- mp-1/calc.js
+++ mp-1/calc.js
@@ -15,8 +15,12 @@ function doSub() { result(Number(one.value) - Number(two.value)); }
 function doMul() { result(Number(one.value) * Number(two.value)); }
 
 function doDiv() {
-  const a = Number(one.value), b = Number(two.value);
-  if (b === 0) { output.innerHTML = "Division by zero is undefined."; output.style.color = ""; return; }
+  const a = Number(one.value);
+  const b = Number(two.value);
+  if (b === 0) { 
+    output.innerHTML = "Division by zero is undefined."; 
+    output.style.color = "red"; 
+    return; }
   result(a / b);
 }
 
@@ -25,7 +29,6 @@ function doPow() {
   const exp   = Number(two.value);
   let r = 1;
   for (let i = 0; i < exp; i++) r *= base;
-  if (exp < 0) r = 1 / r;
   result(r);
 }
 
