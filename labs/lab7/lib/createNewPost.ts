61a5a4d4526d070ec5923a13cef1d07793873931
shreyj03 <jainshrey2004@gmail.com>
2025-11-14 02:03:24 -0500

    lab9

--- labs/lab7/lib/createNewPost.ts
+++ labs/lab7/lib/createNewPost.ts
@@ -1,16 +1,28 @@
 "use server"
 import { PostProps } from "@/types/PostProps"
+import getCollection, {POSTS_COLLECTION} from "@/db";
+
 
 export default async function createNewPost(
-  title: string,
-  content: string
+    title: string,
+    content: string,
 ): Promise<PostProps> {
-  console.log("Create new post");
-  const p = {
-    title: title,
-    content: content,
-    upvotes: 0,
-    downvotes: 0,
-  };
+    console.log("creating new post");
 
-  return { ...p, id: "newId" };}
+    const p = {
+        title: title,
+        content: content,
+        upvotes: 0,
+        downvotes: 0,
+    };
+
+    // insert in DB
+    const postsCollection = await getCollection(POSTS_COLLECTION);
+    const res = await postsCollection.insertOne({ ...p });
+
+    if (!res.acknowledged) {
+        throw new Error("DB insert failed");
+    }
+
+    return { ...p, id: res.insertedId.toHexString() };
+}
--- labs/lab7/lib/getAllPosts.ts
+++ labs/lab7/lib/getAllPosts.ts
@@ -1,6 +1,18 @@
-import mockData from "@/mock.json"
+// import mockData from "@/mock.json"
 import { PostProps } from "@/types/PostProps"
+import getCollection, {POSTS_COLLECTION} from "@/db";
 
 export default async function getAllPosts(): Promise<PostProps[]> {
-  return mockData.mockPosts;
+    const postsCollection = await getCollection(POSTS_COLLECTION);
+    const data = await postsCollection.find().toArray();
+
+    const posts: PostProps[] = data.map((p) => ({
+        id: p._id.toHexString(),
+        title: p.title,
+        content: p.content,
+        upvotes: p.upvotes,
+        downvotes: p.downvotes,
+    }));
+
+    return posts.reverse();
 }
